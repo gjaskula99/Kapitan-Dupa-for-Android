67d419dceb8cec8bfbdabdbbc7464d9fdb2c3e37
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-07-20 11:03:18 +0200

    Dźwięk działa

--- app/src/main/java/com/example/kapitandupa/GameActivity.kt
+++ app/src/main/java/com/example/kapitandupa/GameActivity.kt
@@ -32,6 +32,8 @@ class GameActivity : AppCompatActivity() {
         val piotrek = this.findViewById<ImageView>(R.id.piotrek)
         points += 1
         Log.d("GAME", "Points set to $points")
+        //var mediaPlayer = MediaPlayer.create(this, R.raw.rypanie)
+        //mediaPlayer.start()
         dupa.setImageResource(R.drawable.dupa_2)
         piotrek.setImageResource(R.drawable.pioterk_2)
         val handler = Handler()
@@ -39,6 +41,45 @@ class GameActivity : AppCompatActivity() {
             dupa.setImageResource(R.drawable.dupa_1)
             piotrek.setImageResource(R.drawable.piotrek_1)
         }, 100)
+    }
+
+    fun playSound() {
+        val min = 1
+        val max = 5
+        val random: Int = Random().nextInt(max - min + 1) + min
+        var delay : Long = 13000
+        Log.d("AUDIO", "Next playing rypanie for random $random")
+        var mediaPlayer: MediaPlayer? = null
+        var mKeepPlaying : Boolean = true
+        when(random) {
+            1 -> {mediaPlayer = MediaPlayer.create(this, R.raw.rypanie_jakbabe)
+                 //delay = 10000
+            }
+            2 -> {mediaPlayer = MediaPlayer.create(this, R.raw.rypanie_kolba)
+                 //delay = 16000
+            }
+            3 -> {mediaPlayer = MediaPlayer.create(this, R.raw.rypanie_nie)
+                 //delay = 7000
+            }
+            4 -> {mediaPlayer = MediaPlayer.create(this, R.raw.rypanie_nieczuje)
+                 //delay = 8000
+            }
+            5 -> {mediaPlayer = MediaPlayer.create(this, R.raw.rypanie_trututututu)
+                 //delay = 16000
+            }
+        }
+        val mHandler = Handler()
+        mHandler.postDelayed(Runnable {
+            if (isFinishing) {
+                // Check if the Activity is finishing.
+                return@Runnable
+            }
+            mediaPlayer!!.start()
+            if (mKeepPlaying) {
+                // play the sound again in 10 seconds
+                playSound()
+            }
+        }, delay)
     }
 
     override fun onResume() {
@@ -49,9 +90,7 @@ class GameActivity : AppCompatActivity() {
         Log.d("AUDIO", "Media player started - raw.start")
         Log.d("AUDIO", mediaPlayer.isPlaying.toString())
 
-        val min = 20
-        val max = 80
-        val random: Int = Random().nextInt(max - min + 1) + min
+        playSound()
     }
 
 }
